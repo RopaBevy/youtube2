e1fe8d4261f970f51e9f2d79c1565717ac4de8e9
Ropah <61303301+RopaBevy@users.noreply.github.com>
2022-04-11 11:51:29 -0400

    updated the executable path

--- datacollection.py
+++ datacollection.py
@@ -15,7 +15,7 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path='driver/chromedriver',chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path='./driver/chromedriver',chrome_options=chrome_options)
 
 desired_cap = chrome_options.to_capabilities()
 
